0e5c9aaf6f96ef8c98e74c3ec30107ae31c6cac6
qualityparking <quality.cloud@yahoo.com>
2025-06-25 12:26:46 +0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
+from database import init_db
 from routes.payment_routes import payment_bp
 
 app = Flask(__name__)
 CORS(app)
 
-# Register route
+# DB
+init_db(app)
+
+# Routes
 app.register_blueprint(payment_bp, url_prefix='/api')
 
 if __name__ == '__main__':
