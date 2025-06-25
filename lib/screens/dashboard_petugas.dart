327233f43ca20e8de5cff6d1142b0b6f0a2aad2a
qualityparking <quality.cloud@yahoo.com>
2025-06-25 12:06:08 +0700

    Update dashboard_petugas.dart

--- lib/screens/dashboard_petugas.dart
+++ lib/screens/dashboard_petugas.dart
@@ -1,21 +1,39 @@
 import 'package:flutter/material.dart';
 import 'validate_screen.dart';
+import '../services/auth_service.dart';
+import 'login_screen.dart';
 
 class DashboardPetugasScreen extends StatelessWidget {
   const DashboardPetugasScreen({super.key});
 
+  void _logout(BuildContext context) async {
+    await AuthService.logout();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => const LoginScreen()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Dashboard Petugas')),
+      appBar: AppBar(
+        title: const Text('Dashboard Petugas'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () => _logout(context),
+          ),
+        ],
+      ),
       body: Center(
         child: ElevatedButton.icon(
           icon: const Icon(Icons.search),
-          label: const Text('Validasi Kendaraan'),
+          label: const Text('Validasi Plat Kendaraan'),
           onPressed: () {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => const ValidateScreen()),
+              MaterialPageRoute(builder: (_) => const ValidateScreen()),
             );
           },
         ),
