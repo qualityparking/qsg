cb8eb732dadd25242cc4fef9b2223710ae57fc23
qualityparking <quality.cloud@yahoo.com>
2025-06-25 11:25:19 +0700

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -6,3 +6,21 @@ static Future<Map<String, dynamic>> validatePlat(String plat) async {
   );
   return jsonDecode(res.body);
 }
+
+static Future<String> createPaymentToken(int amount) async {
+  final token = await _getToken();
+  final res = await http.post(
+    Uri.parse('$baseUrl/payment/token'),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token',
+    },
+    body: jsonEncode({
+      'amount': amount,
+      'description': 'Perpanjang Member',
+    }),
+  );
+
+  final data = jsonDecode(res.body);
+  return data['snap_url'];
+}
